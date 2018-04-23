2c395673f0da65cb0aebce5414aa19b3ca21dd8a
Sergi Mascaró <msk1416@gmail.com>
2018-04-23 17:10:34 +0200

    game class created

--- CP_Sudoku/View/src/application/play/Game.java
+++ CP_Sudoku/View/src/application/play/Game.java
@@ -1,6 +1,37 @@
 package application.play;
 
+import com.cp.elems.*;
+import com.cp.solver.BacktrackingSudokuSolver;
 
 public class Game {
+    private String difficulty;
+    //easy --> 9 cells
+    //medium --> 6
+    //hard --> 4
+    private SudokuBoard solution;
+    
+    public Game(String diff) {
+        this.difficulty = diff;
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        solution = solver.fillBoard();
+        if (solution.isResolved()) {
+            
+        }
+    }
+    
+    private void setInitialBoard() {
+        int initialCells = 0;
+        if (difficulty.equals("Easy")) {
+            initialCells = 9;
+        } else if (difficulty.equals("Medium")) {
+            initialCells = 6;
+        } else if (difficulty.equals("Hard")) {
+            initialCells = 4;
+        } else {
+            return;
+        }
+        for (int i = 0; i < initialCells; i++)
+    }
+    
     
 }
