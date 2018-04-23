5249a30a724a1e7e84fb1d829c50d28d8299f0fe
Sergi Mascaró <msk1416@gmail.com>
2018-04-23 21:17:53 +0200

    added sudoku gridpane

--- CP_Sudoku/View/src/application/play/GameController.java
+++ CP_Sudoku/View/src/application/play/GameController.java
@@ -2,6 +2,7 @@ package application.play;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import com.cp.elems.SudokuBoard;
@@ -9,10 +10,13 @@ import com.cp.elems.SudokuBoard;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GameController {
@@ -21,7 +25,8 @@ public class GameController {
     Label lblTitle;
     @FXML
     Button backBtn;
-    
+    @FXML
+    GridPane boardGrid;
     private Game currentGame;
     private String difficulty;
     
@@ -29,6 +34,36 @@ public class GameController {
         lblTitle.setText(lblTitle.getText().replaceFirst("-DIFF-", difficulty));
         this.difficulty = difficulty;
         this.currentGame = game;
+        initBoard();
+    }
+    
+    private void initBoard() {
+        int SIZE = 9;
+        int length = SIZE;
+        int width = SIZE;
+   
+
+        for(int y = 0; y < length; y++){
+            for(int x = 0; x < width; x++){
+
+                Random rand = new Random();
+                int rand1 = rand.nextInt(2);
+
+                // Create a new TextField in each Iteration
+                TextField tf = new TextField();
+                tf.setPrefHeight(50);
+                tf.setPrefWidth(50);
+                tf.setAlignment(Pos.CENTER);
+                tf.setEditable(false);
+                tf.setText("(" + rand1 + ")");
+
+                // Iterate the Index using the loops
+                /*boardGrid.setRowIndex(tf,y);
+                boardGrid.setColumnIndex(tf,x);    
+                boardGrid.getChildren().add(tf);*/
+                boardGrid.add(tf, y, x);
+            }
+        }
     }
 
     
